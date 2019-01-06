51562aa9bdbf9a1f86d85d8c41b50600d4f892dc
appledora <immobile.imbecile0010@gmail.com>
2019-01-06 17:27:00 +0600

    PopUp window added

--- app/src/main/java/com/thegorgeouscows/team/finalrev/FoodFragment.java
+++ app/src/main/java/com/thegorgeouscows/team/finalrev/FoodFragment.java
@@ -1,10 +1,9 @@
 package com.thegorgeouscows.team.finalrev;
 
 
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
@@ -18,7 +17,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -94,7 +92,6 @@ public class FoodFragment extends Fragment {
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.i("my TAG in single", "got here cancelled");
             }
         });
 
