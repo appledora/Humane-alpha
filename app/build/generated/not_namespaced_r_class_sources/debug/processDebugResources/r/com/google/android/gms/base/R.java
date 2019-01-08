e5c6045fa1ed2c08a9df9036829e7baa0a50be10
istiakshihab <shihabistiak@gmail.com>
2019-01-08 17:53:16 +0600

    Fixed Volunteer Registration and Added Password Recovery Mechanism

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/base/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/base/R.java
@@ -62,15 +62,15 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int adjust_height = 0x7f0a0023;
-        public static final int adjust_width = 0x7f0a0024;
-        public static final int auto = 0x7f0a0029;
-        public static final int dark = 0x7f0a0068;
-        public static final int icon_only = 0x7f0a009b;
-        public static final int light = 0x7f0a00a9;
-        public static final int none = 0x7f0a00ce;
-        public static final int standard = 0x7f0a012a;
-        public static final int wide = 0x7f0a0164;
+        public static final int adjust_height = 0x7f0a0024;
+        public static final int adjust_width = 0x7f0a0025;
+        public static final int auto = 0x7f0a002a;
+        public static final int dark = 0x7f0a0069;
+        public static final int icon_only = 0x7f0a009c;
+        public static final int light = 0x7f0a00aa;
+        public static final int none = 0x7f0a00cf;
+        public static final int standard = 0x7f0a012b;
+        public static final int wide = 0x7f0a0166;
     }
     public static final class string {
         private string() {}
